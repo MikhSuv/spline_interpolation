dc34a457e4fb7b2415ce3c5c0d6afe24c8c3ca1a
MikhSuv <mischasuv@mail.ru>
2026-05-08 14:12:11 +0300

    Add command line argument

--- app/main.f90
+++ app/main.f90
@@ -3,10 +3,15 @@ program main
   use spline_interpolation
   implicit none
 
+  character(len=256) :: line
   real(dp), allocatable, dimension(:) :: A, B, P, X, Y
-  integer :: q
+  integer :: q = 100
 
-  q = 100
+  if (COMMAND_ARGUMENT_COUNT() == 1) then
+    call GET_COMMAND_ARGUMENT(1, line)
+    read(line, *) q
+  end if
+
 
   call read_data("data.dat", A, B, P)
   call interpolate_spline(A, B, P, q, X, Y)
